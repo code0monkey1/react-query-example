1f6a986b78edbee374e393861d4777a0ae659be7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-03-06 14:39:30 +0530

    tanstack-query-react
    Example of to use tanstack query  + react query Dev tools to manage server state in a react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+
+import './App.css';
 function App() {
+   const client = useQueryClient();
+   
+   const demo=[{id:1,content:"this"},{id:2,content:"is"},{id:3,content:"a"},{id:4,content:"test"}]
+
+   const {data:posts,isLoading,isError,error} = useQuery({
+    queryKey:["posts"],
+    queryFn:()=> wait(1000).then(res => demo),
+    // this will load as placeholder , before the query
+    placeholderData:[{id:1,content:"awesome stuff"},{id:2,content:"coming your way"}]
+  })
+
+ const {mutate,isLoading:mutationIsLoading}=useMutation({
+    mutationFn:(content)=> demo.push({content,id:Math.floor(10000*Math.random())}),
+   
+   onSuccess:()=>{
+     client.invalidateQueries(["posts"])
+   },
+    onError:()=>{
+        console.error("Error mutating!")
+    }
+  },
+  )
+
+  const wait = (ms) => {
+     
+      return new Promise((resolve, reject) =>{
+            setTimeout(() =>resolve(), ms)
+        })
+  }
+
+  if(isError){
+    return <div>{error}</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+       {posts.map(post =><div key={post.id}>{post.content}</div>)}
+       <button disabled={mutationIsLoading}  onClick={()=> mutate("new content")}>Add Post</button>
+      </ul>
     </div>
   );
 }
